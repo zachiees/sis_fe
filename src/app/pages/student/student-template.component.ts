0d4a718f1ef0a54d21a3150a981389e942795df2
Zach Madrilejos <v-zamadrillejos@meralco.com.ph>
2023-07-10 03:22:25 +0800

    student section placeholder

---
+++ src/app/pages/student/student-template.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-student-template',
+  templateUrl: './student-template.component.html',
+  styleUrls: ['./student-template.component.scss']
+})
+export class StudentTemplateComponent {
+
+}
---
+++ src/app/pages/student/student.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { StudentTemplateComponent } from './student-template.component';
+
+
+
+@NgModule({
+  declarations: [
+    StudentTemplateComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class StudentModule { }
