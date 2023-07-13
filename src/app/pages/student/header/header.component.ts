254026f29639827b27826ce8b23aee719a6063f0
Zach Madrilejos <v-zamadrillejos@meralco.com.ph>
2023-07-14 04:52:33 +0800

    sidebar toggle

--- src/app/pages/student/header/header.component.ts
+++ src/app/pages/student/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component, EventEmitter, Output} from '@angular/core';
 
 @Component({
   selector: 'student-header',
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent {
+  @Output() OnSidebarToggle: EventEmitter<void> = new EventEmitter<void>();
 
+  toggleSidebar(){
+    this.OnSidebarToggle.emit();
+  }
 }
